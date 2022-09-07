75bf9642341304f8c0d9708458ffc524e371d6d0
Anton <leonardspbox@gmail.com>
2022-09-07 18:43:18 +0500

    ...

--- ui/src/components/Folder/components/hooks/useUpload.tsx
+++ ui/src/components/Folder/components/hooks/useUpload.tsx
@@ -20,110 +20,28 @@ export interface UploadResponse {
 
 const useUpload = (dir: string) => {
   const [visible, setVisible] = useState(false);
-  const [report, setReport] = React.useState<Required<UploadResponse>["result"] | null>(null);
-  const [error, setError] = React.useState<null | Error>(null);
+  const [report, setReport] = useState<Required<UploadResponse>["result"] | null>(null);
   const [progress, setProgress] = useState(0);
   const [isRetry, setRetry] = useState(false);
-  const [ok, setOk] = React.useState(false);
-
-  const upload = useCallback(async (dir: string, files: File[]) => {
-    const sumBytes = files.reduce((r, file) => r += file.size, 0);
-    let uploadedBytes = 0;
-    const updateProgress = (bytes: number) => {
-      uploadedBytes += bytes;
-      setProgress(100 / sumBytes * uploadedBytes);
-    };
-
-    const sendChunk = async (key: string, chunk: Blob, pos: number) => {
-      const data = new FormData();
-      data.append("key", key);
-      data.append("pos", String(pos));
-      data.append("size", String(chunk.size));
-      data.append("chunk", chunk);
-
-      await fetch('/~/upload/chunk', {
-        method: 'POST',
-        body: data,
-      }).then(handleApiResponse);
-
-      updateProgress(chunk.size);
-    };
-
-    const uploadFile = async (file: File) => {
-      const {key, chunkSize} = await doReq<{key: string, chunkSize: number}>('/~/upload/init', {
-        fileName: file.name,
-        size: file.size,
-        place: dir,
-      });
-
-      const blob = new Blob([file]);
-      let pos = 0;
-      while (pos < blob.size) {
-        const chunk = blob.slice(pos, pos + chunkSize);
-
-        while (true) {
-          try {
-            await sendChunk(key, chunk, pos);
-
-            break;
-          } catch (error) {
-            const err = error as Error | ApiError;
-            if (['ApiError', 'HTTPError'].includes(err.name)) {
-              throw err;
-            }
-            setRetry(true);
-            await new Promise(r => setTimeout(r, 5 * 1000));
-          }
-        }
-        setRetry(false);
-
-        pos += chunkSize;
-      }
-    };
-
-    const results: UploadFileResult[] = [];
-
-    for (let i = 0, file; file = files[i]; i++) {
-      let error = '';
-      try {
-        await uploadFile(file);
-      } catch (err) {
-        error = (err as Error).message;
-      }
-
-      results.push({
-        ok: !error,
-        filename: file.name,
-        error: error,
-      });
-    }
-
-    return results;
-  }, []);
+  const [ok, setOk] = useState(false);
 
   const resetState = useCallback(() => {
     setReport(null);
-    setError(null);
     setProgress(0);
     setRetry(false);
     setOk(false);
   }, []);
 
-  const handleUpload = React.useCallback(async (files: File[]) => {
+  const handleUpload = useCallback(async (files: File[]) => {
     if (visible && !ok) return;
     resetState();
     setVisible(true);
-    try {
-      const results = await upload(dir, files);
-      setOk(results.every(file => file.ok));
-      setReport(results);
-    } catch (err) {
-      console.error('Upload error: %O', err);
-      setError(err as Error);
-    }
+    const results = await upload(dir, files, setProgress, setRetry);
+    setOk(results.every(file => file.ok));
+    setReport(results);
   }, [dir, upload, ok, visible]);
 
-  const handleClose = React.useCallback((e: SyntheticEvent, reason?: string) => {
+  const handleClose = useCallback((e: SyntheticEvent, reason?: string) => {
     e.preventDefault();
     if (!ok && reason === 'backdropClick') return;
     setVisible(false);
@@ -134,7 +52,7 @@ const useUpload = (dir: string) => {
   if (visible) {
     dialog = (
       <MyDialog fullWidth={true} onClose={handleClose} open={true}>
-        {error || report ? (
+        {report ? (
           <DialogTitle>
             <Box alignItems="center" display="flex">
               Upload {ok ? 'complete' : 'error'}
@@ -147,13 +65,11 @@ const useUpload = (dir: string) => {
         <DialogContent>
           {report ? (
             <Report report={report}/>
-          ) : error ? (
-            <Input fullWidth={true} value={error.message} readOnly/>
           ) : (
             <LinearProgress color={isRetry ? "warning" : "primary"} variant={"determinate"} value={progress}/>
           )}
         </DialogContent>
-        {error || report ? (
+        {report ? (
           <DialogActions>
             <Button onClick={handleClose}>Close</Button>
           </DialogActions>
@@ -163,6 +79,81 @@ const useUpload = (dir: string) => {
   }
 
   return {handleUpload, dialog};
+};
+
+const upload = async (dir: string, files: File[], setProgress: (val: number) => void, setRetry: (bool: boolean) => void) => {
+  const sumBytes = files.reduce((r, file) => r += file.size, 0);
+  let uploadedBytes = 0;
+  const updateProgress = (bytes: number) => {
+    uploadedBytes += bytes;
+    setProgress(100 / sumBytes * uploadedBytes);
+  };
+
+  const sendChunk = async (key: string, chunk: Blob, pos: number) => {
+    const data = new FormData();
+    data.append("key", key);
+    data.append("pos", String(pos));
+    data.append("size", String(chunk.size));
+    data.append("chunk", chunk);
+
+    await fetch('/~/upload/chunk', {
+      method: 'POST',
+      body: data,
+    }).then(handleApiResponse);
+
+    updateProgress(chunk.size);
+  };
+
+  const uploadFile = async (file: File) => {
+    const {key, chunkSize} = await doReq<{key: string, chunkSize: number}>('/~/upload/init', {
+      fileName: file.name,
+      size: file.size,
+      place: dir,
+    });
+
+    const blob = new Blob([file]);
+    let pos = 0;
+    while (pos < blob.size) {
+      const chunk = blob.slice(pos, pos + chunkSize);
+
+      while (true) {
+        try {
+          await sendChunk(key, chunk, pos);
+
+          break;
+        } catch (error) {
+          const err = error as Error | ApiError;
+          if (['ApiError', 'HTTPError'].includes(err.name)) {
+            throw err;
+          }
+          setRetry(true);
+          await new Promise(r => setTimeout(r, 5 * 1000));
+        }
+      }
+      setRetry(false);
+
+      pos += chunkSize;
+    }
+  };
+
+  const results: UploadFileResult[] = [];
+
+  for (let i = 0, file; file = files[i]; i++) {
+    let error = '';
+    try {
+      await uploadFile(file);
+    } catch (err) {
+      error = (err as Error).message;
+    }
+
+    results.push({
+      ok: !error,
+      filename: file.name,
+      error: error,
+    });
+  }
+
+  return results;
 };
 
 export default useUpload;
