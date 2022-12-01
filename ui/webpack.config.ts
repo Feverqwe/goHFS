f5cf3a72c1b064edf5b1242a284419ca20b7c543
Anton <leonardspbox@gmail.com>
2022-12-01 08:56:25 +0500

    fix ui dev

--- ui/webpack.config.ts
+++ ui/webpack.config.ts
@@ -3,87 +3,85 @@ import * as HtmlWebpackPlugin from "html-webpack-plugin";
 import * as Path from "path";
 import * as CopyPlugin from "copy-webpack-plugin";
 
-export default (env: Record<string, any>, arg: Record<string, any>): Configuration => {
-  const isProduction = arg.mode === 'production';
-
-  return {
-    entry: {
-      folder: './src/folder',
-      player: './src/player',
-    },
-    output: {
-      filename: '[name]-[contenthash].js',
-      chunkFilename: '[contenthash].chunk.js',
-      publicPath: isProduction ? '/~/www' : '',
-      path: Path.resolve(__dirname, './dist'),
-    },
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            name: 'commons',
-            chunks: 'initial',
-            minChunks: 2,
-          },
+const config: Configuration = {
+  entry: {
+    folder: './src/folder',
+    player: './src/player',
+  },
+  output: {
+    filename: '[name]-[contenthash].js',
+    chunkFilename: '[contenthash].chunk.js',
+    publicPath: '/~/www',
+    path: Path.resolve(__dirname, './dist'),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: 'commons',
+          chunks: 'initial',
+          minChunks: 2,
         },
       },
     },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [{
-            loader: 'ts-loader',
-          }],
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.png$/,
-          use: [{
-            loader: 'url-loader',
-          }],
-        },
-        {
-          test: /\.less$/,
-          use: [
-            {
-              loader: 'style-loader',
-            }, {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-              },
-            }, {
-              loader: 'less-loader',
-              options: {
-                sourceMap: true,
-              },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{
+          loader: 'ts-loader',
+        }],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png$/,
+        use: [{
+          loader: 'url-loader',
+        }],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          }, {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
             },
-          ],
-        },
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: 'folder.html',
-        template: './src/assets/folder.html',
-        chunks: ['folder'],
-        scriptLoading: 'blocking',
-      }),
-      new HtmlWebpackPlugin({
-        filename: 'player.html',
-        template: './src/assets/player.html',
-        chunks: ['player'],
-        scriptLoading: 'blocking',
-      }),
-      new CopyPlugin({
-        patterns: [
-          { from: "./src/assets/icons", to: "icons" },
+          }, {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
         ],
-      }),
-    ],
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-  };
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'folder.html',
+      template: './src/assets/folder.html',
+      chunks: ['folder'],
+      scriptLoading: 'blocking',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'player.html',
+      template: './src/assets/player.html',
+      chunks: ['player'],
+      scriptLoading: 'blocking',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/assets/icons", to: "icons" },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
 };
+
+export default config;
