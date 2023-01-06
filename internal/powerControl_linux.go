ad910629a507fa42faa06929a83e13604a9c6029
Anton <leonardspbox@gmail.com>
2023-01-06 07:11:29 +0500

    1

--- internal/powerControl_linux.go
+++ internal/powerControl_linux.go
@@ -5,11 +5,11 @@ package internal
 type PowerControl struct {
 }
 
-func (self *PowerControl) Inc() {
+func (s *PowerControl) Inc() {
 
 }
 
-func (self *PowerControl) Dec() {
+func (s *PowerControl) Dec() {
 
 }
 
