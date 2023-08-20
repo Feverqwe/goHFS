f21649b0be48ab6566289f15c14a0a73aca65049
Anton <Feverqwe@users.noreply.github.com>
2023-08-20 10:50:09 +0500

    rm openAt

--- ui/src/components/Player/components/Video/Video.tsx
+++ ui/src/components/Player/components/Video/Video.tsx
@@ -206,9 +206,6 @@ const Video: FC<VideoProps> = ({url, metadata}) => {
 
     const disposeLoadedMetadata = addEvent(video, (on) => on('loadedmetadata', () => {
       disposeLoadedMetadata();
-      api.storageSet({
-        [`${sid}.openAt`]: Math.trunc(Date.now() / 1000),
-      });
       if (refStartTime.current > 0) {
         video.currentTime = refStartTime.current;
       }
