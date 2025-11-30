f66ed6d2b9b04c47f07b1a0dbe1fd5c5d1337b56
Anton <Feverqwe@users.noreply.github.com>
2025-12-01 00:31:08 +0300

    upd

--- internal/boltStorage/boltStorage.go
+++ internal/boltStorage/boltStorage.go
@@ -16,32 +16,29 @@ type BoltStorage struct {
 func (s *BoltStorage) GetKeys(keys []string) map[string]interface{} {
 	var err error
 	var result = make(map[string]interface{})
+
 	if keys == nil {
 		err = s.db.View(func(tx *bolt.Tx) error {
 			b := tx.Bucket(s.bucket)
-			b.ForEach(func(k, v []byte) error {
+			return b.ForEach(func(k, v []byte) error {
 				key := string(k)
-				r, err := JSONToInterface(v)
-				if err != nil {
-					log.Println("Parse key value error", key, err)
-				} else {
+				if r, err := JSONToInterface(v); err == nil {
 					result[key] = r
+				} else {
+					log.Println("parse key value error", key, err)
 				}
 				return nil
 			})
-			return nil
 		})
 	} else {
 		err = s.db.View(func(tx *bolt.Tx) error {
 			b := tx.Bucket(s.bucket)
 			for _, key := range keys {
-				v := b.Get([]byte(key))
-				if v != nil {
-					r, err := JSONToInterface(v)
-					if err != nil {
-						log.Println("Parse key value error", err)
-					} else {
+				if v := b.Get([]byte(key)); v != nil {
+					if r, err := JSONToInterface(v); err == nil {
 						result[key] = r
+					} else {
+						log.Println("parse key value error", key, err)
 					}
 				}
 			}
@@ -54,91 +51,68 @@ func (s *BoltStorage) GetKeys(keys []string) map[string]interface{} {
 	return result
 }
 
-func (s *BoltStorage) GetKey(key string) (interface{}, bool) {
-	var r interface{}
-	err := s.db.View(func(tx *bolt.Tx) error {
+func (s *BoltStorage) GetKey(key string) (r interface{}, err error) {
+	r = nil
+	err = s.db.View(func(tx *bolt.Tx) (err error) {
 		b := tx.Bucket(s.bucket)
-		v := b.Get([]byte(key))
-		if v != nil {
-			var err error
-			r, err = JSONToInterface(v)
-			if err != nil {
-				return fmt.Errorf("parse key value error %s %w", key, err)
+		if v := b.Get([]byte(key)); v != nil {
+			if r, err = JSONToInterface(v); err != nil {
+				err = fmt.Errorf("parse key %s value error: %w", key, err)
 			}
 		}
-		return nil
+		return
 	})
-	if err != nil {
-		log.Println("GetKey single key error", key, err)
-	}
-	return r, err == nil
+	return
 }
 
-func (s *BoltStorage) SetKey(key string, value interface{}) {
-	err := s.db.Update(func(tx *bolt.Tx) error {
+func (s *BoltStorage) SetKey(key string, value interface{}) error {
+	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucket)
 		v, err := InterfaceToJSON(value)
 		if err != nil {
-			return fmt.Errorf("serialize key error %s %w", key, err)
+			return fmt.Errorf("serialize key %s value error: %w", key, err)
 		}
-		err = b.Put([]byte(key), v)
-		if err != nil {
+		if err = b.Put([]byte(key), v); err != nil {
 			return fmt.Errorf("put key error %s %w", key, err)
 		}
 		return nil
 	})
-	if err != nil {
-		log.Println("SetKey key error", key, err)
-	}
 }
 
 func (s *BoltStorage) SetObject(keyValue map[string]interface{}) error {
-	err := s.db.Update(func(tx *bolt.Tx) error {
+	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucket)
 		for key, value := range keyValue {
 			v, err := InterfaceToJSON(value)
 			if err != nil {
-				log.Println("serialize key error", key, err)
+				log.Println(fmt.Errorf("serialize key %s value error: %w", key, err))
 				continue
 			}
-			err = b.Put([]byte(key), v)
-			if err != nil {
-				log.Println("put key error", key, err)
+			if err = b.Put([]byte(key), v); err != nil {
+				log.Println("put key error:", key, err)
 			}
 		}
 		return nil
 	})
-	if err != nil {
-		log.Println("SetObject error", err)
-	}
-	return nil
 }
 
-func (s *BoltStorage) DelKey(key string) {
-	err := s.db.Update(func(tx *bolt.Tx) error {
+func (s *BoltStorage) DelKey(key string) error {
+	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucket)
 		return b.Delete([]byte(key))
 	})
-	if err != nil {
-		log.Println("Del key error", err)
-	}
 }
 
 func (s *BoltStorage) DelKeys(keys []string) error {
-	err := s.db.Update(func(tx *bolt.Tx) error {
+	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucket)
 		for _, key := range keys {
-			err := b.Delete([]byte(key))
-			if err != nil {
+			if err := b.Delete([]byte(key)); err != nil {
 				log.Println("del key error", err)
 			}
 		}
 		return nil
 	})
-	if err != nil {
-		log.Println("DelKeys error", err)
-	}
-	return nil
 }
 
 func (s *BoltStorage) Load() error {
@@ -158,8 +132,7 @@ func (s *BoltStorage) Load() error {
 }
 
 func (s *BoltStorage) Sync() error {
-	err := s.db.Sync()
-	return err
+	return s.db.Sync()
 }
 
 func GetStorage(path string) *BoltStorage {
