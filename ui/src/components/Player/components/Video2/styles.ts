08565356708504a2fd494ca80b14bb818243ac00
Anton <Feverqwe@users.noreply.github.com>
2026-08-13 09:34:29 +0300

    feat: refactor Video2 styles for improved maintainability and consistency

--- ui/src/components/Player/components/Video2/styles.ts
+++ ui/src/components/Player/components/Video2/styles.ts
@@ -1,6 +1,10 @@
 import {styled} from '@mui/material';
 import {alpha} from '@mui/material/styles';
 
+const CONTROL_BAR_HEIGHT = '54px';
+const CONTROL_TARGET_MARGIN = '5px';
+const CONTROL_TARGET_SIZE = '44px';
+
 const getSubtitleShadow = (color: string) =>
   [
     '0.03em 0 0.03em',
@@ -51,10 +55,6 @@ const PlayerContainer = styled('div')(({theme}) => ({
       textShadow: 'none',
     },
 
-  '.video-js .vjs-control:focus .vjs-svg-icon': {
-    filter: 'none',
-  },
-
   '.video-js.vjs-user-inactive.vjs-playing': {
     cursor: 'none',
   },
@@ -70,10 +70,10 @@ const PlayerContainer = styled('div')(({theme}) => ({
     backgroundColor: alpha(theme.palette.primary.main, 0.88),
     color: theme.palette.primary.contrastText,
     lineHeight: '2em',
+    transition: 'background-color 120ms ease-out, color 120ms ease-out, transform 120ms ease-out',
   },
 
-  '.video-js:hover .vjs-big-play-button, .video-js .vjs-big-play-button:focus': {
-    borderColor: theme.palette.primary.light,
+  '.video-js .vjs-big-play-button:hover, .video-js .vjs-big-play-button:focus-visible': {
     backgroundColor: theme.palette.primary.light,
     color: theme.palette.primary.contrastText,
   },
@@ -87,7 +87,7 @@ const PlayerContainer = styled('div')(({theme}) => ({
   },
 
   '.video-js .vjs-control-bar': {
-    height: '4.5em',
+    height: CONTROL_BAR_HEIGHT,
     padding: '0 0.8em',
     alignItems: 'center',
     boxSizing: 'border-box',
@@ -105,29 +105,28 @@ const PlayerContainer = styled('div')(({theme}) => ({
     },
   },
 
-  '.video-js.vjs-layout-small .vjs-control-bar, .video-js.vjs-layout-x-small .vjs-control-bar': {
+  '.video-js:is(.vjs-layout-small, .vjs-layout-x-small) .vjs-control-bar': {
     paddingRight: 0,
     paddingLeft: 0,
   },
 
-  '.video-js.vjs-layout-small .vjs-control.vjs-button, .video-js.vjs-layout-x-small .vjs-control.vjs-button':
-    {
-      marginRight: '0.25em',
-      marginLeft: '0.25em',
-    },
+  '.video-js:is(.vjs-layout-small, .vjs-layout-x-small) .vjs-control.vjs-button': {
+    marginRight: '3px',
+    marginLeft: '3px',
+  },
 
   '.video-js.vjs-layout-small .vjs-time-control, .video-js.vjs-layout-small .vjs-time-divider': {
     fontSize: '1.25em',
   },
 
   '.video-js .vjs-control': {
-    width: '4.5em',
+    width: CONTROL_BAR_HEIGHT,
   },
 
   '.video-js .vjs-control.vjs-button': {
-    width: '3.667em',
-    height: '3.667em',
-    margin: '0.4165em',
+    width: CONTROL_TARGET_SIZE,
+    height: CONTROL_TARGET_SIZE,
+    margin: CONTROL_TARGET_MARGIN,
   },
 
   '.video-js .vjs-button:focus-visible, .video-js .vjs-slider:focus-visible': {
@@ -136,9 +135,9 @@ const PlayerContainer = styled('div')(({theme}) => ({
     boxShadow: `0 0 0 2px ${alpha(theme.palette.common.black, 0.45)}`,
   },
 
-  '.video-js .vjs-button > .vjs-icon-placeholder::before': {
+  '.video-js .vjs-control-bar .vjs-button > .vjs-icon-placeholder::before': {
     fontSize: '2.4em',
-    lineHeight: 1.458,
+    lineHeight: CONTROL_TARGET_SIZE,
   },
 
   '.video-js .vjs-settings-button': {
@@ -160,7 +159,7 @@ const PlayerContainer = styled('div')(({theme}) => ({
     padding: '0',
   },
 
-  '.video-js  .vjs-volume-horizontal': {
+  '.video-js .vjs-volume-control.vjs-volume-horizontal': {
     margin: '0.8em 0',
   },
 
@@ -205,52 +204,6 @@ const PlayerContainer = styled('div')(({theme}) => ({
     },
   },
 
-  '.video-js .vjs-menu-button-popup .vjs-menu': {
-    zIndex: 3,
-    bottom: '2.8em',
-    marginBottom: '1.2em',
-  },
-
-  '.video-js .vjs-menu-button-popup.vjs-hover, .video-js .vjs-menu-button-popup:focus-within': {
-    zIndex: 3,
-  },
-
-  '.video-js .vjs-menu-button-popup .vjs-menu .vjs-menu-content': {
-    bottom: 0,
-    borderRadius: '0.2em',
-    backgroundColor: alpha(theme.palette.background.paper, 0.92),
-    fontFamily: theme.typography.fontFamily,
-  },
-
-  '.video-js .vjs-playback-rate.vjs-menu-button-popup .vjs-menu .vjs-menu-content': {
-    width: 'auto',
-  },
-
-  '.video-js .vjs-subs-caps-button .vjs-menu, .video-js .vjs-audio-button .vjs-menu': {
-    right: 0,
-    left: 'auto',
-    width: 'max-content',
-    minWidth: '12em',
-    maxWidth: 'calc(100vw - 2.4em)',
-  },
-
-  '.video-js .vjs-menu li': {
-    justifyContent: 'flex-start',
-    padding: '0.45em 0.8em',
-    fontSize: '1.2em',
-    textAlign: 'left',
-    textTransform: 'none',
-  },
-
-  '.video-js .vjs-menu li.vjs-menu-item:hover': {
-    backgroundColor: theme.palette.action.hover,
-  },
-
-  '.video-js .vjs-menu li.vjs-selected': {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-  },
-
   '.video-js .vjs-time-tooltip, .video-js .vjs-volume-tooltip': {
     backgroundColor: alpha(theme.palette.background.paper, 0.92),
     color: theme.palette.text.primary,
@@ -264,10 +217,6 @@ const PlayerContainer = styled('div')(({theme}) => ({
 
   '.video-js .vjs-mouse-display .vjs-time-tooltip': {
     top: '-2.8em',
-  },
-
-  '.video-js .vjs-progress-control:hover .vjs-mouse-display .vjs-time-tooltip': {
-    backgroundColor: alpha(theme.palette.background.paper, 0.9),
   },
 
   '.video-js .vjs-volume-control:hover .vjs-volume-tooltip, .video-js .vjs-volume-control:hover .vjs-progress-holder:focus .vjs-volume-tooltip':
@@ -306,12 +255,12 @@ const PlayerContainer = styled('div')(({theme}) => ({
   '.video-js .vjs-progress-control': {
     position: 'absolute',
     zIndex: 2,
-    top: '-1.8335em',
+    top: '-22px',
     right: '1.2em',
     left: '1.2em',
     display: 'flex',
     width: 'auto',
-    height: '3.667em',
+    height: CONTROL_TARGET_SIZE,
   },
 
   '.video-js .vjs-progress-control .vjs-progress-holder': {
@@ -338,25 +287,14 @@ const PlayerContainer = styled('div')(({theme}) => ({
   },
 
   '@media (prefers-reduced-motion: reduce)': {
-    '.video-js .vjs-volume-panel, .video-js .vjs-volume-control, .video-js .vjs-custom-subtitles': {
-      transition: 'none',
-    },
+    '.video-js .vjs-big-play-button, .video-js .vjs-control-bar, .video-js .vjs-volume-panel, .video-js .vjs-volume-control, .video-js .vjs-custom-subtitles':
+      {
+        transition: 'none',
+      },
 
     '.video-js.vjs-mobile-touch .vjs-big-play-button:active': {
       transform: 'none',
     },
-  },
-
-  '.vjs-playback-rate > .vjs-menu-button': {
-    lineHeight: '4.5em',
-  },
-
-  '.vjs-playback-rate .vjs-playback-rate-value': {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '1.4em',
-    lineHeight: 1,
   },
 
   '.vjs-text-track-display': {
@@ -376,7 +314,7 @@ const PlayerContainer = styled('div')(({theme}) => ({
     textAlign: 'center',
     textShadow: getSubtitleShadow(theme.palette.common.black),
     pointerEvents: 'none',
-    transition: 'margin 0.3s',
+    transition: 'margin-bottom 180ms ease-out',
 
     p: {
       margin: 0,
@@ -404,17 +342,12 @@ const PlayerContainer = styled('div')(({theme}) => ({
     left: '0.625em',
     color: theme.palette.common.white,
     fontSize: '1.65em',
-    transition: 'margin 0.2s',
-  },
-
-  '.vjs-user-inactive.vjs-playing .vjs-notice': {
-    marginTop: 0,
   },
 
   '.vjs-notice-text': {
     padding: '0.32em 0.64em',
     borderRadius: '0.13em',
-    background: alpha(theme.palette.background.paper, 0.9),
+    backgroundColor: alpha(theme.palette.background.paper, 0.9),
     fontSize: '0.875em',
     userSelect: 'all',
   },
